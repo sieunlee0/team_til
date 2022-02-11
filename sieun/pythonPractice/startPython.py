cc937518edc080db01ff307ad9dccd8e5f5f8d19
sieunlee0 <dltldmsm@gmail.com>
2022-02-11 22:17:32 +0900

    start ptyhon-quiz1

---
+++ sieun/pythonPractice/startPython.py
@@ -0,0 +1,13 @@
+print("hell0 world");
+# 주석 연습
+'''주석
+연습
+2'''
+# 주석
+# 연습
+# 3
+
+#The quiz of the lecture1
+station = "사당"
+# print(station + "행 열차가 들어오고 있습니다")
+print(station, "행 열차가 들어오고 있습니다")
